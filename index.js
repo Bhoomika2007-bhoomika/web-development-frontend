0b7a8ddc3bbc2c1971e85207b966c93396536c44
Bhoomika2007-bhoomika <tbhoomika247@gmail.com>
2025-03-15 10:56:06 +0530

    Add files via upload
     Sir this are my web development projects

---
+++ index.js
@@ -0,0 +1,56 @@
+// console.log("hello world")
+// let name='bhoomika';
+// let age=18;
+// console.log(name);
+// console.log(age);
+
+// let firstname='bhoomika';
+// let lastname='t';
+// name=firstname+" "+lastname;
+// console.log(name);
+
+// let name="bhoomika";
+// let age=18;
+// let isapproved=false;
+// let firstname=undefined;
+// let selectedcolor=null;
+
+// let person={
+//     name:"bhoomika",
+//     age:18
+// };
+// console.log(person)
+
+//     person.name="bhoomika";
+//     person['age']=18;
+//     console.log(person);
+//     console.log(person.name);
+//     console.log(person.age);
+
+//     let selectedfruits=['apple','grapes','kiwi']
+//     selectedfruits[3]='orange'
+//     console.log(selectedfruits)
+//     console.log(selectedfruits.length)
+
+//     function square(number){
+//         return number*number;
+//     }
+//     function cube(number){
+//         return number*number*number;
+//     }
+//     console.log(square(5));
+//     console.log(cube(5));
+
+//     function addition(number){
+//         return number+number;
+//     }
+// console.log(addition(5))
+// function sub(number){
+//     return number-number;
+// }
+// console.log(sub(5)) 
+
+window.alert('your 50% data is over')
+
+document.getElementById("myh1").textcontent="welcome" name ;
+document.getElementById("myp").textcontent="i like JS";
